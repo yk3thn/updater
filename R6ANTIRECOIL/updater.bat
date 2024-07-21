849d089e93b01affc826073d0d4b9f273b0ef1d1
3thn <65327271+yk3thn@users.noreply.github.com>
2024-07-21 10:17:50 -0400

    Add files via upload

---
+++ R6ANTIRECOIL/updater.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "scriptName=%~nx0"
+set "keepFolder=R6ANTIRECOIL"
+
+for %%F in (*.*) do (
+    if /I not "%%F" == "%scriptName%" del "%%F"
+)
+
+for /d %%D in (*) do (
+    if /I not "%%D" == "%keepFolder%" rd /s /q "%%D"
+)
+
+if exist "%keepFolder%" (
+    for %%F in ("%keepFolder%\*") do move "%%F" .
+    rd /s /q "%keepFolder%"
+)
+
+if exist "R6ANTIRECOIL.exe" (
+    start "" "R6ANTIRECOIL.exe"
+) else (
+    echo R6ANTIRECOIL.exe not found!
+)
+
+endlocal
